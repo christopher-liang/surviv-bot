e9797b9fad7272fd62aa898a63b3fe3dfcecef2e
Tafsiu314 <mkpeterchen@gmail.com>
2018-07-13 17:10:00 -0700

    This is a test
    hai
    ~Pierre

---
+++ Cards.py
@@ -0,0 +1,10 @@
+class Cards:
+	def __init__(self, rankInput, suitInput):
+		self.rank = rankInput
+		self.suit = suitInput
+
+	def getRank(self):
+		return self.rank
+
+	def getSuit(self):
+		return self.suit
