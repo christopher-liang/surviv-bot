530838f6e86f47dd74cdab6bb42a45b73a85c5fa
Oysterclam <andyy219@gmail.com>
2018-07-22 20:27:34 -0700

    added function to get game length in seconds from death screen

--- Main.py
+++ Main.py
@@ -42,7 +42,16 @@ def printStats():
         print(driver.find_element_by_xpath("//*[@id='ui-stats-info-box']/div/div[3]").text)
         print(driver.find_element_by_xpath("//*[@id='ui-stats-info-box']/div/div[4]").text)
         print(driver.find_element_by_xpath("//*[@id='ui-stats-info-box']/div/div[5]").text)
-        
+ 
+ def gameLength():
+        #get game time in seconds from death screen 
+        time = driver.find_element_by_xpath("//*[@id='ui-stats-info-box']/div/div[5]/div[2]").text
+        minutePos= time.find('m')
+        if minutePos==-1:
+            return(time[0:len(time)-1])
+        else: 
+            return(int(time[0:minutePos])*60+int(time[minutePos+2:len(time)-1]))
+                   
 
 driver.get(surviv_url)
 
