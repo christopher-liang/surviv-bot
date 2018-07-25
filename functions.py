6bfce08e9d1d14a5f17310b68a0476cfaaae4a50
Oysterclam <andyy219@gmail.com>
2018-07-25 16:04:31 -0700

    forgot to check this change

--- functions.py
+++ functions.py
@@ -36,3 +36,65 @@ def game_length(self):
         return(int(time[0:len(time)-1]))
     else:
         return(int(time[0:minutePos])*60+int(time[minutePos+2:len(time)-1]))
+
+def weapon_names(self):
+    #list of weapon names, [slot1,slot2,Fists,grenade name]
+    return self.find_elements_by_class_name("ui-weapon-name").text
+        
+def current_weapon(self):
+    #i could only find the opacity of the highlighting box as an indicator of current weapon 
+    if self.find_element_by_xpath(
+            "//*[@id='ui-weapon-id-1']").value_of_css_property("opacity")=="1":
+        return self.find_element_by_xpath(
+            "//*[@id='ui-weapon-id-1']/div[1]").text
+    elif self.find_element_by_xpath(
+            "//*[@id='ui-weapon-id-2']").value_of_css_property("opacity")=="1":
+        return self.find_element_by_xpath(
+            "//*[@id='ui-weapon-id-2']/div[1]").text
+    elif self.find_element_by_xpath(
+            "//*[@id='ui-weapon-id-4']").value_of_css_property("opacity")=="1":
+        return self.find_element_by_xpath(
+            "//*[@id='ui-weapon-id-4']/div[1]").text
+    else:
+        return "Fists"
+
+def inventory(self):
+    #return list of amount of each object in inventory in order 
+    #not sure about where deagle ammo is located
+    inv=self.find_elements_by_class_name("ui-loot-count")
+    inv=map(lambda x: int(x.text), inv)
+    return inv
+
+def health(self):
+    #return health on scale from 0-100
+    
+    styleString = self.find_element_by_xpath("//*[@id='ui-health-actual']").get_attribute("style")
+    if styleString =="":
+        #return if health html was not loaded yet, it tends to load slowly sometimes
+        return -1
+    else:
+        return float(styleString[styleString.find("width: ")+7:styleString.find("%")])
+
+    # scrapped regex code might revive it later
+    # try:
+    #     return float(re.search(r'width:\s(\d*\.?\d*)%',self.find_element_by_xpath("//*[@id='ui-health-actual']").get_attribute("style")).group(1))
+    # except:
+    #     return -1
+  
+def current_clip(self):
+    return int(self.find_element_by_xpath("//*[@id='ui-current-clip'])").text)
+
+def current_remaining_ammo(self):
+    return int(self.find_element_by_xpath("//*[@id='ui-remaining-ammo']").text)
+
+def players_remaining(self):
+    return int(self.find_element_by_xpath("//*[@id='ui-alive-info']/div[2]").text)
+
+def kills(self):
+    killString = self.find_element_by_xpath("//*[@id='ui-kill-count']").text
+    #note: text is empty if you haven't killed anyone yet, but also becomes empty for a fraction of a second while kill number updates...
+    if killString=="":
+        return 0
+    else:
+        return int(killString[0:killString.find(" ")])
+
